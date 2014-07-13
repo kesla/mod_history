76161bfca68a0c4b75a137fbc2db5ca1622f5235
David Björklund <david.bjorklund@gmail.com>
2014-07-13 19:39:36 +0200

    rewrite a bit, be smarter about id:s

--- mod_history.js
+++ mod_history.js
@@ -8,32 +8,36 @@ breach.init(function () {
 
   breach.module('core').on('tabs:state', function (state) {
 
-    var loadingStates = Object.keys(state)
-          .map(function (key) { return state[key] })
-          .filter(function (obj) { return obj.loading }) || []
+    Object.keys(state)
+      .forEach(function (tabId) {
+        var tabState = state[tabId]
 
-    loadingStates.forEach(function (state) {
-      state.entries.forEach(function (entry) {
+        if (!tabState.loading)
+          return
 
-        // win32 time to unix-time
-        var timestamp = new Date(entry.timestamp % 8804332800000)
-          , id = entry.id
-          , dir = __dirname + '/data/raw/' + timestamp.toJSON().slice(0, 10)
-          , filename = dir + '/' + timestamp.toJSON() + '-' + id + '.json'
+        tabState.entries.forEach(function (entry) {
+          // win32 time to unix-time
+          var timestamp = new Date(entry.timestamp % 8804332800000)
+              // entry.id reset on restart, so this will uniquely identify
+              // this entry - so taht we can filter potential duplicates
+            , id = tabId + '-' + entry.id
+            , dir = __dirname + '/data/raw/' + timestamp.toJSON().slice(0, 10)
+            , filename = dir + '/' + timestamp.toJSON() + '-' + entry.id + '.json'
 
-        mkdirp(dir, function () {
-          fs.writeFile(
-              filename
-            , JSON.stringify({
-                  url: entry.virtual_url
-                , timestamp: timestamp
-                , title: entry.title
-                , id: id
-              })
-          )
+          mkdirp(dir, function () {
+            fs.writeFile(
+                filename
+              , JSON.stringify({
+                    url: entry.virtual_url
+                  , timestamp: timestamp
+                  , title: entry.title
+                  , id: id
+                })
+            )
+          })
+
         })
       })
-    })
 
   })
 
