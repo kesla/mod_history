aaf7513a59778c56a087316894159ba5f4bcc55e
David Björklund <david.bjorklund@gmail.com>
2014-07-13 15:26:19 +0200

    initial commit

---
+++ mod_history.js
@@ -0,0 +1,37 @@
+var breach = require('breach_module')
+  , db = require('level')(
+        __dirname + '/db'
+      , { valueEncoding: 'json' }
+    )
+  , getRange = require('level-get-range')(db)
+
+breach.init(function () {
+  breach.register('core', 'tabs:.*');
+
+  breach.module('core').on('tabs:state', function (state) {
+
+    var loadingStates = Object.keys(state)
+          .map(function (key) { return state[key] })
+          .filter(function (obj) { return obj.loading }) || []
+      , batch = db.batch()
+
+
+    loadingStates.forEach(function (state) {
+      state.entries.forEach(function (entry) {
+        batch.put(
+            entry.timestamp
+          , entry
+        )
+      })
+    })
+
+    batch.write(function () {
+      console.log('yo, wrote some data')
+      getRange(function (err, range) {
+        console.log(JSON.stringify(range, null, '\t'))
+      })
+    })
+
+  })
+
+})
