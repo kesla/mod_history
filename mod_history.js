5ff58f48c29e43b1da823bb7ed661049860d2e6b
David Björklund <david.bjorklund@gmail.com>
2014-07-13 21:14:33 +0200

    only record the most recent entry

--- mod_history.js
+++ mod_history.js
@@ -9,41 +9,40 @@ breach.init(function () {
 
   breach.module('core').on('tabs:state', function (state) {
 
-    Object.keys(state)
-      .forEach(function (tabId) {
-        var tabState = state[tabId]
+    Object.keys(state).forEach(function (tabId) {
+      var tabState = state[tabId]
 
-        if (!tabState.loading)
-          return
+      if (!tabState.loading || tabState.entries.length === 0)
+        return
 
-        tabState.entries.forEach(function (entry) {
+      // only record the last entry
+      var entry = tabState.entries[tabState.entries.length - 1]
           // win32 time to unix-time
-          var timestamp = new Date(entry.timestamp % 8804332800000)
-            , dir = __dirname + '/data/raw/' + timestamp.toJSON().slice(0, 10)
-            , filename = dir + '/' + timestamp.toJSON() + '.json'
-            , title = entry.title
-            , id = tabId + entry.id
+        , timestamp = new Date(entry.timestamp % 8804332800000)
+        , dir = __dirname + '/data/raw/' + timestamp.toJSON().slice(0, 10)
+        , filename = dir + '/' + timestamp.toJSON() + '.json'
+        , title = entry.title
+        , id = tabId + entry.id
 
-          // check that the id isn't already set, cause we sometimes get the
-          //  same page twice - seem mostly to happen whenever there's a site
-          //  that uses the History-API
-          //  also, this means that the same file won't be saved multiple times
-          if (entry.title && entry.title.length > 0 && !savedCache.has(id)) {
-            savedCache.set(id, true)
-            mkdirp(dir, function () {
-              fs.writeFile(
-                  filename
-                , JSON.stringify({
-                      url: entry.url.href
-                    , timestamp: timestamp
-                    , title: entry.title
-                  })
-              )
-            })
-          }
-
+      // check that the id isn't already set, cause we sometimes get the
+      //  same page twice - seem mostly to happen whenever there's a site
+      //  that uses the History-API
+      //  also, this means that the same file won't be saved multiple times
+      if (entry.title && entry.title.length > 0 && !savedCache.has(id)) {
+        savedCache.set(id, true)
+        mkdirp(dir, function () {
+          fs.writeFile(
+              filename
+            , JSON.stringify({
+                  url: entry.url.href
+                , timestamp: timestamp
+                , title: entry.title
+              })
+          )
         })
-      })
+      }
+
+    })
 
   })
 
